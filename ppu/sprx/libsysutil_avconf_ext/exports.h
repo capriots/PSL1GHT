7a54b3b23b49a56ac62c1062cc9f0af2b1df4f08
capriots <29807355+capriots@users.noreply.github.com>
2022-05-30 10:46:43 +0200

    fix function names

--- ppu/sprx/libsysutil_avconf_ext/exports.h
+++ ppu/sprx/libsysutil_avconf_ext/exports.h
@@ -1,26 +1,26 @@
 #ifndef __EXPORTS_H__
 #define __EXPORTS_H__
 
-EXPORT(cellAudioOutUnregisterDevice, 0x0BDDEF7D);
-EXPORT(cellAudioOutGetDeviceInfo2, 0x18D0ADA6);
-EXPORT(cellVideoOutSetXVColor, 0x1CFA1A11);
-EXPORT(cellVideoOutSetupDisplay, 0x269FFEDD);
-EXPORT(cellAudioInGetDeviceInfo, 0x3D5730CE);
-EXPORT(cellVideoOutConvertCursorColor, 0x4EC8C141);
-EXPORT(cellVideoOutGetGamma, 0x655A0364);
-EXPORT(cellAudioInGetAvailableDeviceInfo, 0x65BF9EA3);
-EXPORT(cellAudioOutGetAvailableDeviceInfo, 0xC3273D9B);
-EXPORT(cellVideoOutSetGamma, 0xC7020F62);
-EXPORT(cellAudioOutRegisterDevice, 0xC7087631);
-EXPORT(cellAudioOutSetDeviceMode, 0xE4645AF8);
-EXPORT(cellAudioInSetDeviceMode, 0xEB6C50FB);
-EXPORT(cellAudioInRegisterDevice, 0xEB6FCFF1);
-EXPORT(cellAudioInUnregisterDevice, 0xFA6BCC17);
-EXPORT(cellVideoOutGetScreenSize, 0xFAA275A4);
-EXPORT(cellVideoOutSetCopyControl, 0x1A324E03);
-EXPORT(cellVideoOutConfigure2, 0x9FAA12BE);
-EXPORT(cellAudioOutGetConfiguration2, 0x44D2BA34);
-EXPORT(cellAudioOutConfigure2, 0xFA611DF4);
-EXPORT(cellVideoOutGetResolutionAvailability2, 0x21036F4E);
+EXPORT(audioOutUnregisterDevice, 0x0BDDEF7D);
+EXPORT(audioOutGetDeviceInfo2, 0x18D0ADA6);
+EXPORT(videoOutSetXVColor, 0x1CFA1A11);
+EXPORT(videoOutSetupDisplay, 0x269FFEDD);
+EXPORT(audioInGetDeviceInfo, 0x3D5730CE);
+EXPORT(videoOutConvertCursorColor, 0x4EC8C141);
+EXPORT(videoOutGetGamma, 0x655A0364);
+EXPORT(audioInGetAvailableDeviceInfo, 0x65BF9EA3);
+EXPORT(audioOutGetAvailableDeviceInfo, 0xC3273D9B);
+EXPORT(videoOutSetGamma, 0xC7020F62);
+EXPORT(audioOutRegisterDevice, 0xC7087631);
+EXPORT(audioOutSetDeviceMode, 0xE4645AF8);
+EXPORT(audioInSetDeviceMode, 0xEB6C50FB);
+EXPORT(audioInRegisterDevice, 0xEB6FCFF1);
+EXPORT(audioInUnregisterDevice, 0xFA6BCC17);
+EXPORT(videoOutGetScreenSize, 0xFAA275A4);
+EXPORT(videoOutSetCopyControl, 0x1A324E03);
+EXPORT(videoOutConfigure2, 0x9FAA12BE);
+EXPORT(audioOutGetConfiguration2, 0x44D2BA34);
+EXPORT(audioOutConfigure2, 0xFA611DF4);
+EXPORT(videoOutGetResolutionAvailability2, 0x21036F4E);
 
 #endif
