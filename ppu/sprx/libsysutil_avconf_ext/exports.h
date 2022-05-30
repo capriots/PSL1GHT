55eafb879ab5865df15e30efef2073aa8dc44f77
capriots <29807355+capriots@users.noreply.github.com>
2022-05-30 10:44:37 +0200

    Add exports.h

---
+++ ppu/sprx/libsysutil_avconf_ext/exports.h
@@ -0,0 +1,26 @@
+#ifndef __EXPORTS_H__
+#define __EXPORTS_H__
+
+EXPORT(cellAudioOutUnregisterDevice, 0x0BDDEF7D);
+EXPORT(cellAudioOutGetDeviceInfo2, 0x18D0ADA6);
+EXPORT(cellVideoOutSetXVColor, 0x1CFA1A11);
+EXPORT(cellVideoOutSetupDisplay, 0x269FFEDD);
+EXPORT(cellAudioInGetDeviceInfo, 0x3D5730CE);
+EXPORT(cellVideoOutConvertCursorColor, 0x4EC8C141);
+EXPORT(cellVideoOutGetGamma, 0x655A0364);
+EXPORT(cellAudioInGetAvailableDeviceInfo, 0x65BF9EA3);
+EXPORT(cellAudioOutGetAvailableDeviceInfo, 0xC3273D9B);
+EXPORT(cellVideoOutSetGamma, 0xC7020F62);
+EXPORT(cellAudioOutRegisterDevice, 0xC7087631);
+EXPORT(cellAudioOutSetDeviceMode, 0xE4645AF8);
+EXPORT(cellAudioInSetDeviceMode, 0xEB6C50FB);
+EXPORT(cellAudioInRegisterDevice, 0xEB6FCFF1);
+EXPORT(cellAudioInUnregisterDevice, 0xFA6BCC17);
+EXPORT(cellVideoOutGetScreenSize, 0xFAA275A4);
+EXPORT(cellVideoOutSetCopyControl, 0x1A324E03);
+EXPORT(cellVideoOutConfigure2, 0x9FAA12BE);
+EXPORT(cellAudioOutGetConfiguration2, 0x44D2BA34);
+EXPORT(cellAudioOutConfigure2, 0xFA611DF4);
+EXPORT(cellVideoOutGetResolutionAvailability2, 0x21036F4E);
+
+#endif
