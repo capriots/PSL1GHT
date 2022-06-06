d974ad81b0ebd0223bd22ba98576ac1461b7d980
capriots <29807355+capriots@users.noreply.github.com>
2022-06-06 15:27:36 +0200

    - add audioOut and audioIn functions - add audio_out.h and audio_in.h - rename everything in sysutil from "video..." to "videoOut..." - add stuff to videoOut

--- ppu/sprx/libsysutil_avconf_ext/exports.h
+++ ppu/sprx/libsysutil_avconf_ext/exports.h
@@ -1,26 +1,31 @@
 #ifndef __EXPORTS_H__
 #define __EXPORTS_H__
 
-EXPORT(audioOutUnregisterDevice, 0x0BDDEF7D);
-EXPORT(audioOutGetDeviceInfo2, 0x18D0ADA6);
-EXPORT(videoOutSetXVColor, 0x1CFA1A11);
-EXPORT(videoOutSetupDisplay, 0x269FFEDD);
-EXPORT(audioInGetDeviceInfo, 0x3D5730CE);
-EXPORT(videoOutConvertCursorColor, 0x4EC8C141);
-EXPORT(videoOutGetGamma, 0x655A0364);
-EXPORT(audioInGetAvailableDeviceInfo, 0x65BF9EA3);
-EXPORT(audioOutGetAvailableDeviceInfo, 0xC3273D9B);
-EXPORT(videoOutSetGamma, 0xC7020F62);
-EXPORT(audioOutRegisterDevice, 0xC7087631);
-EXPORT(audioOutSetDeviceMode, 0xE4645AF8);
-EXPORT(audioInSetDeviceMode, 0xEB6C50FB);
-EXPORT(audioInRegisterDevice, 0xEB6FCFF1);
-EXPORT(audioInUnregisterDevice, 0xFA6BCC17);
-EXPORT(videoOutGetScreenSize, 0xFAA275A4);
-EXPORT(videoOutSetCopyControl, 0x1A324E03);
-EXPORT(videoOutConfigure2, 0x9FAA12BE);
-EXPORT(audioOutGetConfiguration2, 0x44D2BA34);
-EXPORT(audioOutConfigure2, 0xFA611DF4);
-EXPORT(videoOutGetResolutionAvailability2, 0x21036F4E);
+/* video output support */
+EXPORT(videoOutSetXVColor, 0x1cfa1a11);
+EXPORT(videoOutSetupDisplay, 0x269ffedd);
+EXPORT(videoOutConvertCursorColor, 0x4ec8c141);
+EXPORT(videoOutGetGamma, 0x655a0364);
+EXPORT(videoOutSetGamma, 0xc7020f62);
+EXPORT(videoOutGetScreenSize, 0xfaa275a4);
+EXPORT(videoOutSetCopyControl, 0x1a324e03);
+EXPORT(videoOutConfigure2, 0x9faa12be);
+EXPORT(videoOutGetResolutionAvailability2, 0x21036f4e);
+
+/* audio output support */
+EXPORT(audioOutUnregisterDevice, 0x0bddef7d);
+EXPORT(audioOutGetDeviceInfo2, 0x18d0ada6);
+EXPORT(audioOutGetAvailableDeviceInfo, 0xc3273d9b);
+EXPORT(audioOutRegisterDevice, 0xc7087631);
+EXPORT(audioOutSetDeviceMode, 0xe4645af8);
+EXPORT(audioOutGetConfiguration2, 0x44d2ba34);
+EXPORT(audioOutConfigure2, 0xfa611df4);
+
+/* audio input support */
+EXPORT(audioInGetDeviceInfo, 0x3d5730ce);
+EXPORT(audioInGetAvailableDeviceInfo, 0x65bf9ea3);
+EXPORT(audioInSetDeviceMode, 0xeb6c50fb);
+EXPORT(audioInRegisterDevice, 0xeb6fcff1);
+EXPORT(audioInUnregisterDevice, 0xfa6bcc17);
 
 #endif
